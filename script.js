d45352619f6ed7101d28e79f8e5f3b6c0d21a6dd
Michal Kozlowski <mcbiggos@gmail.com>
2017-06-09 20:10:26 +0200

    Create script.js

--- script.js
+++ script.js
@@ -12,6 +12,32 @@ function setupCanvas() {
 // change size on resize
 window.addEventListener('resize', setupCanvas);
 
+
+// requestAnimationFrame polyfill
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
 // necessary variables
 	// setup arrays for fireworks and its' particles
 	var particleList = [],
