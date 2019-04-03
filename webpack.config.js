aa933ed260a83863a1385fdf58adb939e81f077f
kozlovvski <mcbiggos@gmail.com>
2019-04-03 23:08:47 +0200

    added html-loader to webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
