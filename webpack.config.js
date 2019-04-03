2dc7d9a861597c042ffb97968a9ca47717157cd7
kozlovvski <mcbiggos@gmail.com>
2019-04-03 23:39:02 +0200

    fixed .git-ignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
       {
         test: /\.html$/,
         use: [
           {
             loader: "html-loader",
-            options: { minimize: true }
+            options: { minimize: false }
           }
         ]
       }
