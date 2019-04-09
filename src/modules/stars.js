4d17f9399b4e48ea18771205d38a6342654aabe6
kozlovvski <mcbiggos@gmail.com>
2019-04-09 15:13:33 +0200

    added help

--- src/modules/stars.js
+++ src/modules/stars.js
@@ -24,20 +24,14 @@ export class Star {
 		this.opacity = 0;
 	}
 	update() {
-		// WATCH OUT - MATHS AND PHYSICS AHEAD
-		// Quadratic formula f
-		// zeros of it are at 0 and max lifespan, so from intercept form: y = a(x - x1)(x - x2):
-		// opacity = at(t - max) where t is current time and max is maximum lifespan
-		// at half of lifespan opacity should be 1 -> half of the lifespan is max / 2
-		// 1 = a * max / 2 * (max / 2 - max)
-		// 1 = - a * max / 2 * max / 2
-		// 4 = - a * max ^ 2 
-		// a = - 4 / max ^ 2 
-		// so our opacity formula is: opacity = -4t(t - max) / max ^ 2
-		// now for any maximum lifespan, maximum opacity is 1
-		this.opacity = -4 * this.life.current * (this.life.current - this.life.target) / Math.pow(this.life.target, 2);
-		// if opacity is negative, remove star from the list
 		this.life.current++;
+
+		// increase star opacity in the beginning and fade it in the end
+		if (this.life.current < 50) {
+			this.opacity = this.life.current / 50 // increases from 0 to 1
+		} else if (this.life.current > this.life.target - 50) {
+			this.opacity = (this.life.target - this.life.current) / 50 // decreases from 1 to 0
+		} else this.opacity = 1;
 	}
 
 	draw() {
